b251367eef2af36d18e6a388b00e2f2ad9eccc2a
km4891 <kmorarity@gmail.com>
2020-12-31 23:53:23 -0800

    minor fixes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -82,7 +82,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  Category.update(req.body, {
+  Category.destroy({
     where: {
       id: req.params.id
     }
